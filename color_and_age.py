bff9bac7d655dde41b3c4111b91a6d61b649047c
tispdev <ryan.palatty@gmail.com>
2025-11-01 20:35:23 -0400

    Create color_and_age.py
    Learned about different data types, how to convert between them. Learned how to use type specifiers.

---
+++ color_and_age.py
@@ -0,0 +1,4 @@
+color = input("Enter your favorite color: ")
+age = int(input("Enter your age: "))
+#print("Your favorite color is " + color +  " and you are " + str(age) + " years old.")
+print("Your favorite color is %s and you are %d years old."%(color,age))
