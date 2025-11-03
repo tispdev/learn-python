b5c30a7ec286b8be0082f092d9da2d830ae9c068
tispdev <ryan.palatty@gmail.com>
2025-11-03 16:56:56 -0500

    Create user_data_collector.py
    This program collects user input on: name, birthyear, and hometown. It then calculates the user's approximate age and prints a sentence.

---
+++ user_data_collector.py
@@ -0,0 +1,7 @@
+currentyear = 2025
+name = input("Enter your name: ")
+birthyear = int(input("Enter your birth year: "))
+hometown = input("Enter your hometown: ")
+age = currentyear - birthyear
+
+print("%s from %s is approximately %d years old."%(name,hometown,age))
