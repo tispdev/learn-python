564fc273c94dc197bf47ee48294203602847765f
tispdev <ryan.palatty@gmail.com>
2025-11-08 17:29:05 -0500

    conditionals first program
    calculates if a number is positive or negative.

---
+++ conditionals_test.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+if (number > 0):
+    print("The number %d is positive!"%(number))
+elif (number == 0):
+    print("The number is 0!")
+else:
+    print("The number %d is not positive!"%(number))
+
+    
+
