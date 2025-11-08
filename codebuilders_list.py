aa214413335efc2318bee969e6500c0a36503456
tispdev <ryan.palatty@gmail.com>
2025-11-08 18:35:44 -0500

    Create codebuilders_list.py
    Learned about lists, and how to access, modify and use them through a loop.

---
+++ codebuilders_list.py
@@ -0,0 +1,23 @@
+#Create and print a list
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+
+
+
+#access by index
+fruits = ["apple", "banana", "orange"]
+print(fruits[0]) #first item
+print(fruits[2]) #third item
+
+
+#modify a list
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+fruits[1] = "mango" #change stored values in an index of a list
+print(fruits)
+
+
+#loop through a list
+fruits = ["apple", "banana", "orange"]
+for i in fruits:
+    print("I like", i)
