0b123912b9344646d260dcc95294702d357fbf9b
tispdev <ryan.palatty@gmail.com>
2025-11-08 17:30:01 -0500

    Create conditional_age_checker.py
    Calculates age to determine whether user is a kid, teenager or adult.

---
+++ conditional_age_checker.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+if age < 13:
+    print("You are a kid!")
+elif age >= 13 and age <= 19:
+    print("You are a teenager!")
+elif age >= 20:
+    print("You are an adult!")
