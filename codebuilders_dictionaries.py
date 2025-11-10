d93315797f1cc732301808e0f076e52381ff4164
tispdev <ryan.palatty@gmail.com>
2025-11-10 18:08:23 -0500

    import random
    learned how to import random

---
+++ codebuilders_dictionaries.py
@@ -0,0 +1,33 @@
+#create and print a dictionary
+
+student = {
+    "name": "Allen",
+    "age": 16,
+    "grade": "11"
+}
+print(student)
+
+
+#Access by key
+
+print(student["name"])
+print(student["age"])
+
+
+#modify a dictionary
+
+student["age"] = 17
+student["subject"] = "Computer Science"
+print(student)
+
+
+#loop through Keys and Values
+    #first way
+for key in student:
+    print(key, "->", student[key])
+
+   #second way
+print("\nUsing .items():")
+for k, v in student.items():
+    print(f"{k}: {v}")
+#first value is the key, the second is the value (key:value)
