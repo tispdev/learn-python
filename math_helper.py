a94d49caee23bfc7a96716cb52d1aebd9e15a2dc
tispdev <ryan.palatty@gmail.com>
2026-01-10 17:42:44 -0500

    Create math_helper.py
    This programs takes a number and doubles it and also check's if it's even or not.

---
+++ math_helper.py
@@ -0,0 +1,10 @@
+def double(x):
+    return 2 * x
+
+def is_even(x):
+    return x % 2 == 0
+
+number = 6
+
+print("Double of", number, "is", double(number))
+print("Is", number, "even?", is_even(number))
