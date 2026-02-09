dce8ad389551c5280d9a0fce1d0fe8e48e59c853
tispdev <ryan.palatty@gmail.com>
2026-02-09 17:24:52 -0500

    Create review_drill.py
    Fixed the bugs in the original program

---
+++ review_drill.py
@@ -0,0 +1,22 @@
+player = "Alex"
+score = 0
+
+print("Welcome", player)
+
+for i in range(1, 4):
+    guess = int(input("Guess (1-10): "))
+    if guess == 7:
+        print("Win!")
+        score = score + 1
+        break
+    else
+        print("Try again")
+
+print("Score: ", score)
+
+def cheer():
+    print("Great job!")
+
+favorites = ["game", "food"]
+print(favorites[1])
+    
